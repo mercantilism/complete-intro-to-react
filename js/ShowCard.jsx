ca1943d33859d61d9be07a4d3e13e497682dadbf
mercantilism <samuel.galen.gardner@gmail.com>
2017-11-22 15:08:40 -0800

    Showcard component

---
+++ js/ShowCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shape, string } from 'prop-types';
+
+const ShowCard = props => (
+  <div className="show-card">
+    <img src={`/public/img/posters/${props.show.poster}`} alt={`${props.show.title} Show Poster`} />
+    <div>
+      <h3>{props.show.title}</h3>
+      <h4>({props.show.year})</h4>
+      <p>{props.show.description}</p>
+    </div>
+  </div>
+);
+
+/* if we wanted to create default prop vals
+ShowCard.defaultProps = {
+  foo: 'stuff'
+};
+*/
+
+// we can define the props of a propTypes type object with the shape() method
+ShowCard.propTypes = {
+  show: shape({
+    poster: string.isRequired,
+    title: string.isRequired,
+    year: string.isRequired,
+    description: string.isRequired
+  }).isRequired
+};
+
+export default ShowCard;
