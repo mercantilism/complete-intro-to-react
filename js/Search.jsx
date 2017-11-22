f01259d443e0f7467f7b248e22b76f01b743e160
mercantilism <samuel.galen.gardner@gmail.com>
2017-11-22 15:08:24 -0800

    Search component

---
+++ js/Search.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import preload from '../data.json';
+import ShowCard from './ShowCard';
+
+// if we wanted to hold an array of html elements from our json we could:
+// const Shows = preload.shows.map(show => <h3>{show.title}</h3>);
+// but it would actually be a bad idea, because if anything changed
+// to do with the actual shows data, Shows would still hold its
+// original assignment value
+
+const Search = () => (
+  <div className="search">
+    {/* the key prop is a unique id for react to hold on to for more efficient diffing */}
+    {preload.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}
+  </div>
+);
+
+export default Search;
