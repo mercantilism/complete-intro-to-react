361a9b837b999d700d83faf0cc7955f966f85ae1
mercantilism <samuel.galen.gardner@gmail.com>
2017-11-29 15:40:48 -0800

    Details component

--- js/App.jsx
+++ js/App.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
+import Details from './Details';
 
 const FourOhFour = () => <h1>404</h1>;
 
@@ -20,6 +21,7 @@ const App = () => (
         {/* <h1> Example of inter jsx comment </h1> */}
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
+        <Route path="/details/:id" component={Details} />
         <Route component={FourOhFour} />
       </Switch>
     </div>
