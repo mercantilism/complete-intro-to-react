9857377c3484838b87c318c358b45aea299b1986
mercantilism <samuel.galen.gardner@gmail.com>
2017-11-30 13:39:11 -0800

    statefull Landing

--- js/landing.jsx
+++ js/landing.jsx
@@ -1,16 +1,26 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const Landing = () => (
-  <div className="landing">
-    <h1>reactvideo</h1>
-    <input type="text" placeholder="search" />
-    <Link to="/search">or Browse All</Link>
-  </div>
-  // the 'to' attribute of the Link component provides the
-  //  corresponding href for a given route
-);
+class Landing extends Component {
+  state = {
+    searchTerm: ''
+  };
+  handleSearchTerm = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+  render() {
+    return (
+      <div className="landing">
+        <h1>reactvideo</h1>
+        <input type="text" placeholder="search" value={this.state.searchTerm} onChange={this.handleSearchTerm} />
+        <Link to="/search">or Browse All</Link>
+      </div>
+      // the 'to' attribute of the Link component provides the
+      //  corresponding href for a given route
+    );
+  }
+}
 
 export default Landing;
