71baa2097da45f0df3ba8ed5a69c18412f4959ef
mercantilism <samuel.galen.gardner@gmail.com>
2017-12-01 12:58:22 -0800

    enabled redux dev tools

--- js/store.js
+++ js/store.js
@@ -1,8 +1,16 @@
 // @flow
 
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  compose(
+    // if the environment is the browser and the redux devToolsExtension is present
+    // then use the extension; else do nothing:
+    // f => f, is an identity function: all it does is return it's argument
+    typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
+  )
+);
 
 export default store;
